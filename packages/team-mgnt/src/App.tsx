126239749007116ff62aab0ddb103213c40af322
Sam Stevens <sam@ctrl-alt-sam.com>
2018-08-01 14:59:51 -0500

    added react redux the typescript way

--- packages/team-mgnt/src/App.tsx
+++ packages/team-mgnt/src/App.tsx
@@ -2,26 +2,53 @@ import * as React from 'react';
 
 import './App.css';
 
-import HelloWorld from 'tsts';
+// import HelloWorld from 'tsts';
 
 import logo from './logo.svg';
 
+// import Hello from './components/Hello';
+
+import Hello from './containers/Hello';
+
+import { Provider } from 'react-redux';
+
+import { enthusiasm } from './reducers/index';
+
+import { IStoreState } from './types/index';
+
+import { createStore } from 'redux';
+
+import { EnthusiasmAction } from './actions/index';
+
+
+const store = createStore<IStoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
+
 class App extends React.Component {
   public render() {
-    console.log(HelloWorld);
+    // console.log(HelloWorld);
     
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <HelloWorld color={"#FF0"} />
+        <div>
+          <Hello />
+        </div>
+        <div>
+        {/* <HelloWorld color={"#FF0"} /> */}
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
 
       </div>
+      </Provider>
     );
   }
 }
